a808376e6aa1e2b723ec94005c58cbfb8694ae8a
hugosenari <hugosenari@gmail.com>
2022-03-12 01:52:15 -0300

    chore: re index jsbind

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Define bindings to JavaScript and Emscripten'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."jsbind-master".url = "path:./master";
+  inputs."jsbind-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jsbind-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Define bindings to JavaScript and Emscripten'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-jsbind-master.flake = false;
+  inputs.src-jsbind-master.owner = "yglukhov";
+  inputs.src-jsbind-master.ref   = "refs/heads/master";
+  inputs.src-jsbind-master.repo  = "jsbind";
+  inputs.src-jsbind-master.type  = "github";
+  
+  inputs."https://github.com/yglukhov/wasmrt".dir   = "nimpkgs/h/https://github.com/yglukhov/wasmrt";
+  inputs."https://github.com/yglukhov/wasmrt".owner = "riinr";
+  inputs."https://github.com/yglukhov/wasmrt".ref   = "flake-pinning";
+  inputs."https://github.com/yglukhov/wasmrt".repo  = "flake-nimble";
+  inputs."https://github.com/yglukhov/wasmrt".type  = "github";
+  inputs."https://github.com/yglukhov/wasmrt".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."https://github.com/yglukhov/wasmrt".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-jsbind-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-jsbind-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
